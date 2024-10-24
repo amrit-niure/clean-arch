c6159a88611e8a1a79a0e14b385e69071a3c8b34
amrit-niure <niureamrit2@gmail.com>
2024-10-24 17:38:49 +1100

    refactor: slight changes

--- src/application/use-cases/sign-up.use-case.ts
+++ src/application/use-cases/sign-up.use-case.ts
@@ -6,47 +6,47 @@ import { ISignUp } from "@/src/entities/models/users";
 import { DatabaseOperationError } from "@/src/entities/errors/common";
 
 export async function signUpUseCase(input: ISignUp): Promise<{ user: User }> {
-    const usersRepository = getInjection("IUsersRepository");
-    
-    const HASH_CONFIG = {
-        memoryCost: 19456,
-        timeCost: 2,
-        outputLen: 32,
-        parallelism: 1,
-    };
+  const usersRepository = getInjection("IUsersRepository");
 
-    try {
-        const user = await usersRepository.getUserByEmail(input.email);
-        if (user) {
-            throw new AuthenticationError("Email already in use.");
-        }
+  const HASH_CONFIG = {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  };
 
-        const hPw = await hash(input.password, HASH_CONFIG);
-
-        const newUser = await usersRepository.createUser({
-            firstName: input.firstName,
-            lastName: input.lastName,
-            middleName: input?.middleName,
-            email: input.email,
-            hashedPassword: hPw,
-            role: input?.role || 'user',
-        });
-
-        return {
-            user: {
-                id: newUser.id,
-                firstName: newUser.firstName,
-                middleName: newUser.middleName,
-                lastName: newUser.lastName,
-                email: newUser.email,
-                emailVerified: newUser.emailVerified,
-                role: newUser.role,
-            }
-        };
-    } catch (err) {
-        if (err instanceof DatabaseOperationError) {
-            throw new Error("Failed to create the user due to a database error.");
-        }
-        throw err;
+  try {
+    const user = await usersRepository.getUserByEmail(input.email);
+    if (user) {
+      throw new AuthenticationError("Email already in use.");
     }
+
+    const hPw = await hash(input.password, HASH_CONFIG);
+
+    const newUser = await usersRepository.createUser({
+      firstName: input.firstName,
+      lastName: input.lastName,
+      middleName: input?.middleName,
+      email: input.email,
+      hashedPassword: hPw,
+      role: input?.role || "user",
+    });
+
+    return {
+      user: {
+        id: newUser.id,
+        firstName: newUser.firstName,
+        middleName: newUser.middleName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+        emailVerified: newUser.emailVerified,
+        role: newUser.role,
+      },
+    };
+  } catch (err) {
+    if (err instanceof DatabaseOperationError) {
+      throw new Error("Failed to create the user due to a database error.");
+    }
+    throw err;
+  }
 }
--- src/application/use-cases/update-user.use-case.ts
+++ src/application/use-cases/update-user.use-case.ts
@@ -2,17 +2,17 @@ import { User } from "@/db/drizzle/schema/users";
 import { getInjection } from "@/di/container";
 
 export async function updateUserUseCase(
-    id: string,
-    input: Partial<User>
+  id: string,
+  input: Partial<User>,
 ): Promise<User | null> {
-    try {
-        const updateUser = getInjection("IUsersRepository")
-        const user: User | null = (await updateUser.updateUser(id, input)) ?? null
-        if (!user) {
-            throw new Error("User not found");
-        }
-        return user
-    } catch (error) {
-        throw new Error("Email verification failed");
+  try {
+    const updateUser = getInjection("IUsersRepository");
+    const user: User | null = (await updateUser.updateUser(id, input)) ?? null;
+    if (!user) {
+      throw new Error("User not found");
     }
+    return user;
+  } catch (error) {
+    throw new Error("Email verification failed");
+  }
 }
