634b3c8c8426278167bee633e2eedddb5b11570a
amrit-niure <niureamrit2@gmail.com>
2024-10-24 13:46:53 +1100

    fix: unauthentication error fixed

--- app/_lib/check-auth.ts
+++ app/_lib/check-auth.ts
@@ -1,40 +1,46 @@
 import { getInjection } from "@/di/container";
-import { UnauthenticatedError, UnauthorizedError } from "@/src/entities/errors/auth";
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+} from "@/src/entities/errors/auth";
 import { redirect } from "next/navigation";
-type Role = 'USER' | 'ADMIN';
+type Role = "USER" | "ADMIN";
 
-
-export async function checkAuth(requiredRole: Role = 'USER'): Promise<void> {
+export async function checkAuth(requiredRole: Role = "USER"): Promise<void> {
   const authenticationService = getInjection("IAuthenticationService");
 
-  const { user, session } = await authenticationService.validateRequest()
+  const { user, session } = await authenticationService.validateRequest();
 
   if (!session) {
-    throw new UnauthenticatedError('Unauthenticated');
+    throw new UnauthenticatedError("Unauthenticated");
   }
 
   if (!user || user.role !== requiredRole) {
-    throw new UnauthorizedError('Unauthorized');
+    throw new UnauthorizedError("Unauthorized");
   }
 }
 
 export async function validateServerProtectedRoute() {
   const authenticationService = getInjection("IAuthenticationService");
+  try {
+    const { user, session } = await authenticationService.validateRequest();
 
-  const { user, session } = await authenticationService.validateRequest()
-  if (!session) {
-    redirect('/signin');
+    if (!session) {
+      redirect("/signin");
+    }
+    return { user, session };
+  } catch (err) {
+    console.log(err);
+    redirect("/signin");
   }
-  return { user, session };
 }
-
 
 export const validateAdminInServer = async () => {
   const authenticationService = getInjection("IAuthenticationService");
 
-  const { user } = await authenticationService.validateRequest()
+  const { user } = await authenticationService.validateRequest();
   if (user?.role !== "ADMIN") {
-    redirect('/');
+    redirect("/");
   }
   return { user };
 };
