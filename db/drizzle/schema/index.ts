658fe8831a9fea50e581f67b859734f31b04cccc
Amrit Niure <niureamrit2@gmail.com>
2024-10-26 19:06:25 +1100

    fix: slight changes

--- db/drizzle/schema/index.ts
+++ db/drizzle/schema/index.ts
@@ -1,6 +1,8 @@
-export { customers, customersRelations } from './customers'
-export {emailVerificationCodes, emailVerificationCodeRealtions} from './email_verification_code'
-export {resetTokens, resetTokenRealtions} from './reset_token'
-export {sessions, sessionsRelations} from './sessions'
-export {users, usersRelations} from './users'
-export {visaApplications, visaApplicationsRelations} from './visa_applications'
+export { customers, customersRelations } from "./customers";
+export { resetTokens, resetTokenRealtions } from "./reset_token";
+export { sessions, sessionsRelations } from "./sessions";
+export { users, usersRelations } from "./users";
+export {
+  visaApplications,
+  visaApplicationsRelations,
+} from "./visa_applications";
