43c0ae35ca7925db452986fef3a7df2e7dda8b81
Amrit Niure <niureamrit2@gmail.com>
2024-10-26 18:59:13 +1100

    fix: unnecessary files deleted

--- di/container.ts
+++ di/container.ts
@@ -5,8 +5,6 @@ import { EmailModule } from "./modules/email.module";
 
 import { DI_RETURN_TYPES, DI_SYMBOLS } from "./types";
 import { UsersModule } from "./modules/users.module";
-import { EmailVerificationCodeModule } from "./modules/email-verification.module";
-
 
 const ApplicationContainer = new Container({
   defaultScope: "Singleton",
@@ -16,14 +14,12 @@ export const initializeContainer = () => {
   ApplicationContainer.load(AuthenticationModule);
   ApplicationContainer.load(UsersModule);
   ApplicationContainer.load(EmailModule);
-  ApplicationContainer.load(EmailVerificationCodeModule);
 };
 
 export const destroyContainer = () => {
   ApplicationContainer.unload(AuthenticationModule);
   ApplicationContainer.unload(UsersModule);
   ApplicationContainer.unload(EmailModule);
-  ApplicationContainer.unload(EmailVerificationCodeModule);
 };
 
 if (process.env.NODE_ENV !== "test") {
@@ -35,4 +31,3 @@ export function getInjection<K extends keyof typeof DI_SYMBOLS>(
 ): DI_RETURN_TYPES[K] {
   return ApplicationContainer.get(DI_SYMBOLS[symbol]);
 }
-
